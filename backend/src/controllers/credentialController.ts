021df1deb87fb04534c4a67116d9497a4bb21941
AkseliA <akseli.aula@gmail.com>
2023-05-04 18:28:18 +0300

    Add backend validations and error handling

--- backend/src/controllers/credentialController.ts
+++ backend/src/controllers/credentialController.ts
@@ -4,14 +4,18 @@ import credentialModel from "../models/credentialModel";
 class CredentialController {
   public async addCredential(req: Request, res: Response, next: NextFunction) {
     try {
-      //Store credential
+      const user = req?.user;
+      const credentialToStore = req.body.credential;
+
+      //Store credential and return stored credential
       const insert = await credentialModel
         .query()
         //@ts-ignore
-        .insert({ user_ref: req.user.id, content: req.body.credential })
+        .insert({ user_ref: user.id, content: credentialToStore })
         .returning("*");
 
-      res.json(insert);
+      //Return stored credential
+      res.status(200).json(insert);
     } catch (err) {
       res.status(500).json({ msg: "Error adding credential" });
     }
@@ -24,33 +28,40 @@ class CredentialController {
     next: NextFunction
   ) {
     try {
-      const user = req.user;
-      const id = req.body.id;
+      const user = req?.user;
+      const credentialId = req.body.id;
+      const updatedCredential = req.body.credential;
+
+      //If credentialId or updatedCredential is not found, throw error
+      if (!credentialId || !updatedCredential) {
+        throw new Error();
+      }
 
       //Get the credential with the supplied id (in req.body)
-      //Also check that the user is the owner of the credential.
+      //Also check that the user is the owner of the credential (where clause).
       const credential = await credentialModel
         .query()
         //@ts-ignore
         .where("user_ref", user.id)
-        .andWhere("id", id);
+        .andWhere("id", credentialId);
 
       if (!credential) {
-        return res.json(404).json({ msg: "Error updating credential" });
+        throw new Error();
       }
 
-      // Update credential
+      // Update and return updated credential.
       const insert = await credentialModel
         .query()
         //@ts-ignore
         .where("user_ref", user.id)
-        .andWhere("id", id)
-        .update({ content: req.body.credential })
+        .andWhere("id", credentialId)
+        .update({ content: updatedCredential })
         .returning("*");
 
+      //Return updated credential
       res.json(insert);
     } catch (err) {
-      res.status(500).json({ msg: "Error updating credential" });
+      res.status(500).json("Error updating credential");
     }
     next();
   }
@@ -63,6 +74,7 @@ class CredentialController {
       const user = req.user;
       const credentialId = req.params.id;
 
+      //Delete the credential, returns an integer that indicated affected rows.
       const deleted = await credentialModel
         .query()
         .delete()
@@ -70,7 +82,7 @@ class CredentialController {
         .where("user_ref", user.id)
         .andWhere("id", credentialId);
 
-      //Returns an integer that indicates affected rows.
+      //Return the integer (affected rows)
       res.json(deleted);
     } catch (err) {
       res.status(500).json("Error deleting credentials");
@@ -81,13 +93,13 @@ class CredentialController {
   public async getCredentials(req: Request, res: Response, next: NextFunction) {
     try {
       const user = req.user;
+
+      //Get all credentials of the user. If no credentials are found, return empty array.
       const credentials = await credentialModel
         .query()
         //@ts-ignore
         .where("user_ref", user.id);
-      if (!credentials) {
-        return res.json(404).json({ msg: "Error getting credentials" });
-      }
+
       res.json({ credentials: credentials });
     } catch (err) {
       res.status(500).json("Error getting credentials");
--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -4,20 +4,29 @@ import userModel from "../models/userModel";
 import jwt from "jsonwebtoken";
 
 //https://cheatsheetseries.owasp.org/cheatsheets/Authentication_Cheat_Sheet.html
-/**@TODO error handling */
 
 class UserController {
   public async createUser(req: Request, res: Response, next: NextFunction) {
     try {
       //parse request body
       const username = req.body.username;
+      if (!username || !isValidUsername(username)) {
+        throw new Error();
+      }
+
+      const password = req.body.password;
+      if (!password || !isValidPassword(password)) {
+        throw new Error();
+      }
+
       //Check if user with same username exists
       const doesUserExist = await userModel.query().findOne({ username });
+
       //User does not exist -> continue registering
       if (!doesUserExist) {
         //Create password salt and hash usign bcrypt
         const salt = bcrypt.genSaltSync(10);
-        let pwHash = await bcrypt.hash(req.body.password, salt);
+        let pwHash = await bcrypt.hash(password, salt);
 
         /**
          * Generate another salt, which is used when deriving vault key using pbkdf2 (client side).
@@ -47,31 +56,30 @@ class UserController {
   public async loginUser(req: Request, res: Response, next: NextFunction) {
     try {
       //parse and validate request body
-      const user = req.body;
+      const password = req.body?.password;
+      const username = req.body?.username;
+      if (!username || !password || !isValidUsername(username)) {
+        throw new Error();
+      }
 
       const userCandidate = await userModel
         .query()
-        .findOne({ username: user.username });
+        .findOne({ username: username });
       if (!userCandidate) {
-        return res
-          .status(401)
-          .json({ msg: "Login failed: invalid credentials." });
+        return res.status(401).json("Login failed: invalid credentials.");
       }
 
       //Remove vaultKeyHash from the latter part of password.
       const passwordHash = userCandidate.password.slice(0, -29);
-      const isValidPassword = await bcrypt.compare(user.password, passwordHash);
+      const isValidPassword = await bcrypt.compare(password, passwordHash);
+
       if (!isValidPassword) {
-        return res
-          .status(401)
-          .json({ msg: "Login failed: invalid credentials." });
+        return res.status(401).json("Login failed: invalid credentials.");
       }
 
       //Create jwt and return token
       const jwtPayload = {
-        id: userCandidate.id,
-        username: userCandidate.username,
-        pwhash: userCandidate.password
+        id: userCandidate.id
       };
 
       const token = jwt.sign(jwtPayload, process.env.JWT_SECRET, {
@@ -85,10 +93,58 @@ class UserController {
         passwordHash: userCandidate.password
       });
     } catch (err) {
-      res.status(500).json("Error logging user");
+      res.status(500).json("Error logging in");
     }
     next();
   }
 }
 
+/**
+ * Function to validate that password meets min requirements when registering an user
+ * bcrypt allows only 72 bytes of data in the hashing, so password is limited to 64 characters to be on the safe side.
+ * Since password is stored as hash ( > 64chars) also the plaintext password length must be checked before proceeding
+ *
+ * Requirements (https://pages.nist.gov/800-63-3/sp800-63b.html Appendix A - A.1):
+ *  - length 8 - 64 characters
+ *  - atleast 1 x digit
+ *  - atleast 1 x special character
+ *  - atleast 1 x uppercase and lowercase letters
+ *
+ * @param password
+ * @returns boolean - indicates if is valid (true) or not valid (false)
+ */
+
+const isValidPassword = (password: string): boolean => {
+  //Length requirement
+  if (password?.length < 8 || password?.length > 64) {
+    console.log("length");
+    return false;
+  }
+  //Regex for checking that password has atleast 1x digit, symbol, upper&lowercase characters
+  const regex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
+  if (!regex.test(password)) {
+    console.log("regex");
+    return false;
+  }
+
+  return true;
+};
+
+/**
+ * Function for checking that the username supplied (in registration) is valid.
+ * When storing the username in database, additional validation is done against defined jsonSchema (in userModel)
+ * Requirements:
+ *  - Length between 5 - 32 characters
+ *
+ * @param username
+ * @returns boolean - indicates if is valid (true) or not valid (false)
+ */
+const isValidUsername = (username: string): boolean => {
+  const trimmed = username.trim(); //remove whitespaces
+  if (trimmed?.length < 5 || trimmed?.length > 32) {
+    return false;
+  }
+  return true;
+};
 export default new UserController();
